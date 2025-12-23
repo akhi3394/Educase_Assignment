fd3c2921631df0ebde0c2cd45aceb917c53fb20c
akhi3394 <akhilvemula3394@gmail.com>
2025-12-23 19:53:36 +0530

    Inputs label alignment fixed

--- src/components/FloatingInput.tsx
+++ src/components/FloatingInput.tsx
@@ -8,18 +8,21 @@ interface FloatingInputProps extends InputHTMLAttributes<HTMLInputElement> {
 const FloatingInput = forwardRef<HTMLInputElement, FloatingInputProps>(
   ({ label, required, id, ...props }, ref) => {
     return (
-      <div className="floating-input-wrapper">
-        <input
-          ref={ref}
-          id={id}
-          className="floating-input peer"
-          placeholder=" "
-          {...props}
-        />
-        <label htmlFor={id} className="floating-label">
-          {label}
-          {required && <span className="text-label">*</span>}
-        </label>
+      <div className="relative w-full">
+        {/* Fieldset creates the border with a gap */}
+        <fieldset className="border border-border rounded-md px-3 h-14">
+          <legend className="px-1 text-sm text-label">
+            {label}
+            {required && <span className="text-label text-[#e45959]">*</span>}
+          </legend>
+
+          <input
+            ref={ref}
+            id={id}
+            className="w-full h-full bg-transparent outline-none text-foreground text-sm"
+            {...props}
+          />
+        </fieldset>
       </div>
     );
   }
